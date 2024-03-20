53e11bf958a4faac964409242965e19e165b1591
harsha-improve10X <shreeharsha@improve10x.com>
2024-03-20 11:22:10 +0530

    Added an ImageView and TextView.

--- app/src/main/java/com/example/imageutilities/SplashActivity.java
+++ app/src/main/java/com/example/imageutilities/SplashActivity.java
@@ -22,5 +22,10 @@ public class SplashActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        displaySplashScreen();
+    }
+
+    private void displaySplashScreen() {
+
     }
 }
